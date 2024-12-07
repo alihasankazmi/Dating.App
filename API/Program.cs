7f2bdadca3d7c15b04c74983ec3e7bdbc49ce1e7
aliha <aliha@DESKTOP-T8L6OFN>
2024-12-07 14:53:02 +0500

    Add Client

--- API/Program.cs
+++ API/Program.cs
@@ -5,12 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins(
+    "https://localhost:4200",
+    "http://localhost:4200"));
 
 app.MapControllers();
 
